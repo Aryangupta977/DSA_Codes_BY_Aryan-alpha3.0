3b96f02eccce6b281ca695987a400ba61bed4d8f
Aryan Gupta <aryangupta92874@gmail.com>
2024-05-21 22:31:52 +0530

    Second  commit

--- src/reverse_int.java
+++ src/reverse_int.java
@@ -1,3 +1,15 @@
 public class reverse_int {
 
+    public static void main(String[] args) {
+        int n = 234;
+        int reverse = 0, extract;
+        while(n<0)
+        {
+            extract = n % 10;
+            reverse = reverse * 10 + extract;
+            n = n/10;
+        }
+
+        System.out.println(reverse);
+    }
 }
